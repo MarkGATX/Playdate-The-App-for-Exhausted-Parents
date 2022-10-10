3d82d3d50f169e001563d94a6a3811b59c829b59
unknown <jimmymhuang@gmail.com>
2022-10-10 17:14:16 -0500

    Initial weather JS edit

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@
 var lat;
 var long;
 var playDateMapBoxToken = 'pk.eyJ1IjoibWFya2dhdHgiLCJhIjoiY2w5MndoNDVqMDEwZDN5bXBiOTZseTYyMSJ9.-ZUmZXLJEzZyTkCwSBMGuw';
-
+// variable for weather icons
+var iconCode;
 
 //Check for geolocation in browser
 if ('geolocation' in navigator) {
@@ -15,7 +16,6 @@ if ('geolocation' in navigator) {
 
 function success(lat, long) {
     logLatLong(lat, long);
-    buildMaps();
   }
   
 function error() {
@@ -32,18 +32,60 @@ const options = {
   //grabs position once 
 navigator.geolocation.getCurrentPosition((position) => {
     success(position.coords.latitude, position.coords.longitude);
+    console.log(lat);
+    console.log(long);
+    var coordinateUrl = queryUrl + lat + '&lon=' + long + '&key=' + weatherAPIKey + '&units=I';
+    console.log(coordinateUrl);
   }, error,options);
 
   // regularly updates position -- if we want to constantly update location
 //   const watchID = navigator.geolocation.watchPosition(success, error, options);
 
 //   navigator.geolocation.clearWatch(watchID); --- stop watching position
-
 function logLatLong (latitude, longitude) {
-    lat= latitude;
+    lat = latitude;
     long = longitude;
-  }
+}
 
   function noGeoSearchMap(){
 
-  };
+};
+
+//weatherbit.io API retrieval
+  var weatherAPIKey = "54d824ecca864b9dbe80b3b774711d3a";
+  var queryUrl = "https://api.weatherbit.io/v2.0/current?lat=";
+//retrieving weather at current location
+const weatherIconList = "https://www.weatherbit.io/api/codes";
+function geolocationWeather(){
+  var coordinateUrl = queryUrl + lat + '&lon=' + long + '&key=' + weatherAPIKey + '&units=I';
+  fetch(coordinateUrl).then(function weatherDataHere() {
+    //create section for weather display
+    var weatherHereEl = $(`<div>`).attr({id: weather-here});
+    //obtain weather icons from API, TBD
+    //var weatherIcon = weatherData.current.weather[0].icon;
+    //var cityWeatherIcon = weatherIcons + weatherIcon + '.jpg';
+    //create display for current location
+    var weatherDisplayEl = $(`<h2>`).text("Your current location")
+    //add in image element for weather icon
+    var weatherIconImg = $(`<img>`).attr({
+      id: 'current-weather-icon',
+      src: weatherIconList,
+      alt: 'Image of simple weather icon',
+    })
+    //create unordered list of desired weather details
+    var weatherListEl = $(`<ul>`)
+    var weatherDetails = [
+      "Temperature: " + data.temp + " °F",
+      "Wind: " + data.wind_spd + " Miles per Hour",
+      "Humidity: " + data.rh + "%",
+      "UV Index: " + data.uv
+    ]
+    for (var x = 0; x < weatherDetails.length; x++){
+      var weatherItems = $(`<li>`).text(weatherDetails[x])
+      weatherListEl.append(weatherItems);
+    }
+    weatherHereEl.append(weatherIconImg);
+    weatherHereEl.append(weatherListEl);
+  })
+}
+geolocationWeather();
